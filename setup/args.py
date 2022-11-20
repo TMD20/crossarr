ee6a37cc8d4612415a11dc8eb16277c1135820bb
Your Name <you@example.com>
2022-11-20 17:22:57 -0600

    fix errors

--- setup/args.py
+++ setup/args.py
@@ -1,18 +1,12 @@
 from jsonargparse import ArgumentParser, ActionConfigFile
-<<<<<<< HEAD
-=======
 from jsonargparse.typing import restricted_number_type,restricted_string_type
->>>>>>> origin/scheduler
 import jsonargparse
 import os
 import pathlib
 
-<<<<<<< HEAD
-=======
 import setup.defaults as defaults
 
 
->>>>>>> origin/scheduler
 Docker_KEY = os.environ.get('CROSSARR_DOCKER', False)
 def setupConfig(p):
     if not Docker_KEY:
@@ -23,14 +17,6 @@ def setupConfig(p):
         defaultconfigPath ="/config/config.json"
         p.default_config_files = [defaultconfigPath]
     return p
-<<<<<<< HEAD
-def setupDir(r):
-    if Docker_KEY:
-        r.log=f"/logs/{r.subcommand}.log"
-        r[r.subcommand].folder="/output"
-    return r
-  
-=======
 
 def postSetup(r):
     r.clientname=r.clientname or defaults.getClientName(r)
@@ -39,23 +25,14 @@ def postSetup(r):
   
    
 
->>>>>>> origin/scheduler
 
 def getArgs():
     p = ArgumentParser(prog="crossarr")
     p=setupConfig(p)
-<<<<<<< HEAD
- 
-  
-    p.add_argument('-l', '--log', help="Where to save log file")
-    p.add_argument('-v', '--loglevel', help="what level to set log to flexible case-senstivity main options are [DEBUG,INFO,OFF]",choices=["Debug","DEBUG","debug","INFO","info","Info","off","OFF","Off"],default="OFF")
-    p.add_argument("-r","--rows",help="Advanced Feature to set how many table rows to render for Messages",default=5)
-=======
 
     p.add_argument('-n', '--clientname', help="Name of client use for logfile/lockfile\nsonarr or raddarr use if not set",required=False)
     p.add_argument('-v', '--loglevel', help="what level to set log to flexible case-senstivity main options are [DEBUG,INFO,OFF]",type=restricted_string_type('LOG Names', '(off|debug|info)'),default="OFF")
     p.add_argument("-r","--rows",help="Advanced Feature to set how many table rows to render for Messages")
->>>>>>> origin/scheduler
     p.add_argument("-t","--threshold",type=int,default=1,help="The max size difference \% a match can have")
     p.add_argument('-d', '--days', type=int,default=99999999999999999999,help="Max Age of a release in days")
     p.add_argument('-a', '--prowlarrapi', help="Prowlar API key")
@@ -67,10 +44,7 @@ def getArgs():
     imported= Releases completed, and imported into library
     """
     )
-<<<<<<< HEAD
-=======
     p.add_argument("-it","--interval",default=defaults.getDefaultInterval(),help="Run the script every x minutes, 0 turns off",type=interval,metavar="[0-44640]")
->>>>>>> origin/scheduler
     p.add_argument("-i",'--indexers', nargs="+",help="Names of Indexer\nUses Regex to Match Names")
 
     radarr=ArgumentParser()
@@ -88,21 +62,9 @@ def getArgs():
     subcommands.add_subcommand('sonarr', sonarr)
 
 
-<<<<<<< HEAD
-    r=p.parse_args()
-    subspace=r.get(r.subcommand)
-    if not subspace.api:
-        raise jsonargparse.ParserError(f"{r.subcommand}.api","can't be null")
-    if not subspace.folder:
-        raise jsonargparse.ParserError(f"{r.subcommand}.folder","can't be null")
-    if not subspace.url:
-        raise jsonargparse.ParserError(f"{r.subcommand}.url","can't be null")    
-    return setupDir(r)
-=======
    
     r=p.parse_args()  
     return  postSetup(r)
->>>>>>> origin/scheduler
     
   
     
--- setup/logger.py
+++ setup/logger.py
@@ -15,22 +15,9 @@ def logfilter(record):
 def nologfilter(record):
     return False
     
-<<<<<<< HEAD
-def setupLog():
-    userargs=args.getArgs()
-    if not userargs.get("log"):
-        userargs.log=os.path.join(pathlib.Path(os.path.realpath(__file__)).parents[1], f"{userargs.subcommand}.log")
-    pathlib.Path(pathlib.Path(userargs.log).parents[0].name).mkdir(parents=True, exist_ok=True)
-
-    
-    if userargs.loglevel.upper()!="OFF":
-        console.logging.getLogger(name=None).filter=logfilter
-        console.logging.basicConfig(filename=userargs.log, encoding='utf-8', level=getattr(console.logging, userargs.loglevel.upper()),format='%(asctime)s:%(levelname)s:%(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-=======
 def setupLogging():
     if userargs.loglevel.upper()!="OFF":
         console.logging.getLogger(name=None).filter=logfilter
         console.logging.basicConfig(filename=os.path.join(defaults.getLogFolder(),userargs.clientname) ,level=getattr(console.logging, userargs.loglevel.upper()),format='%(asctime)s:%(levelname)s:%(message)s',datefmt='%Y-%m-%d %H:%M:%S')
->>>>>>> origin/scheduler
     else:
         console.logging.getLogger(name=None).filter=nologfilter
